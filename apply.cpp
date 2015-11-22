b9e85fa25a86f4b34a821b6feebc37106e19913f
Cosmin Boaca <cosmin.boaca1994@gmail.com>
2015-11-22 17:18:17 -0500

    Apply works on non-void functions

--- apply.cpp
+++ apply.cpp
@@ -1,36 +1,61 @@
+/*
+   Similar to f(*list) from python.
+   Limitations: Does not work on template functions
+*/
 #include <functional>
+#include <cassert>
 #include <iostream>
 #include <type_traits>
 #include <utility>
 using namespace std;
 
-template<class Fn, class Tuple, size_t... I>
-void apply_impl(Fn fun, Tuple&& argsTuple, index_sequence<I...>) {
-    return fun(get<I>(argsTuple)...);
-}
+namespace detail {
+    template<class Fn, class Tuple, class ReturnType, size_t... I>
+    inline ReturnType
+    apply_impl(Fn fun, Tuple&& argsTuple, const index_sequence<I...>&)
+    {
+        return fun(get<I>(argsTuple)...);
+    }
 
-template<class Fn, class...Args>
-void apply(Fn f, tuple<Args...>&& argsTuple) {
-    apply_impl(f, std::move(argsTuple),
-            make_index_sequence<sizeof...(Args)>());
-}
+    template<class Fn, class...Args>
+    inline auto apply(Fn f, tuple<Args...>&& argsTuple) ->
+    typename result_of<Fn(Args...)>::type {
+        using ReturnType = typename result_of<Fn(Args...)>::type;
+        const size_t ArgsLength = sizeof...(Args);
+        return apply_impl<
+                    Fn,
+                    tuple<Args...>,
+                    ReturnType
+                >(f, std::move(argsTuple), make_index_sequence<ArgsLength>());
+    }
+};
 
-void f(int x) {
-    cout << x << endl;
-}
-
-void g(int x, int y, int z) {
+void printSum(int x, int y, int z) {
     cout << (x + y + z) << endl;
 }
 
-void h(int& t) {
+void incrementRef(int& t) {
     t++;
-    cout << t << endl;
+}
+
+int sum(int x, int y) {
+    return x + y;
+}
+
+int& returningRef(int value) {
+    int *x = new int(value);
+    return (*x);
 }
 
 int main() {
+    using detail::apply;
     int x = 0;
-    apply(g, make_tuple(1, 2, 3));
-    apply(h, make_tuple(ref(x)));
+    apply(printSum, make_tuple(1, 2, 3));
+    apply(incrementRef, make_tuple(ref(x)));
+    assert(x == 1);
+    assert(apply(sum, make_tuple(1, 2)) == 3);
+    int& y = apply(returningRef, make_tuple(1));
+    y++;
+    assert(y == 2);
     return 0;
 }
